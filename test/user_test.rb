90f6cac619efdec9ae3848d87e9dece9cc699114
Oscar Bellechasse <obellech10@gmail.com>
2019-05-08 09:44:33 -0600

    Create test_user_is_told_jokes

--- test/user_test.rb
+++ test/user_test.rb
@@ -2,17 +2,20 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/joke'
 require './lib/user'
+require 'pry'
 
 class UserTest < MiniTest::Test
 
   def setup
     @sal = User.new("Sal")
+    @ali = User.new("Ali")
     @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
     @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
   end
 
   def test_that_it_exists
     assert_instance_of User, @sal
+    assert_instance_of User, @ali
   end
 
   def test_that_it_has_attributes
@@ -25,5 +28,12 @@ class UserTest < MiniTest::Test
     @sal.learn(@joke_2)
 
     assert_equal [@joke_1, @joke_2], @sal.jokes
+  end
+
+  def test_user_is_told_jokes
+    @sal.tell(@ali, @joke_1)
+    @sal.tell(@ali, @joke_2)
+    
+    assert_equal [@joke_1, @joke_2], @ali.jokes
   end
 end
