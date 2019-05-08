4d6eed9e134e21d1875054c4ad008b2070556a60
Oscar Bellechasse <obellech10@gmail.com>
2019-05-08 09:30:17 -0600

    Create test_that_user_has_jokes method

--- test/user_test.rb
+++ test/user_test.rb
@@ -7,9 +7,23 @@ class UserTest < MiniTest::Test
 
   def setup
     @sal = User.new("Sal")
+    @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
   end
 
   def test_that_it_exists
     assert_instance_of User, @sal
+  end
+
+  def test_that_it_has_attributes
+    assert_equal "Sal", @sal.name
+    assert_equal [], @sal.jokes
+  end
+
+  def test_that_user_has_jokes
+    @sal.learn(@joke_1)
+    @sal.learn(@joke_2)
+
+    assert_equal [@joke_1, @joke_2], @sal.jokes
   end
 end
